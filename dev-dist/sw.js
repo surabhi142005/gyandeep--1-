14cb798b5c0c770ef55d886b3dcd8ba398bd0fd7
Gyandeep Dev Team <dev@gyandeep.local>
2026-04-16 20:35:42 +0530

    chore: update service worker and settings

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f87553f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/offline.html",
-    "revision": "0.9glumflmqt8"
+    "revision": "0.tqjpbd36tf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
